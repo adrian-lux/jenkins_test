918bc46bb64c970129cec6e223306cf08a14837c
s2-jenkins <mcaubrey511+jenkins@gmail.com>
2018-08-30 04:52:09 -0400

    ......

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,7 +11,7 @@ node {
         /* This builds the actual image; synonymous to
          * docker build on the command line */
 
-        app = docker.build("s2/hellonode")
+        app = docker.build("adrian-lux/jenkins_test")
     }
 
     stage('Test image') {
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello s2\n");
 });
 
 // listen on localhost:8000
